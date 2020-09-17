efcd40e7b75beb3c15a252c939b11e16e66e9428
Sergio Cano <serdeliverance@gmail.com>
2020-09-17 19:38:55 -0300

    Sortable interface

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,10 +1,16 @@
 import { NumbersCollection } from './NumbersCollection'
 
+interface Sortable {
+  length: number
+  compare(leftIndex: number, rightIndex: number): boolean
+  swap(leftIndex: number, rightIndex: number): void
+}
+
 export class Sorter {
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort(): void {
-    let length = this.collection.length
+    let { length } = this.collection
     for (let i = 0; i < length; i++) {
       for (let j = 0; j < length - i - 1; j++) {
         if (this.collection.compare(j, j + 1)) {
