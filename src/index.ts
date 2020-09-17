518c7212e1ac784824d44ac9c4120297786ec4c0
Sergio Cano <serdeliverance@gmail.com>
2020-09-17 09:02:16 -0300

    NumberCollection interface and refactor

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,8 @@
-class Sorter {
-    constructor(public collection: number[]) {
-    }
+import { Sorter } from './Sorter'
+import { NumbersCollection } from './NumbersCollection'
 
-    sort(): void {
-        let length = this.collection.length
-        for (let i = 0; i < length; i++) {
-            for (let j = 0; j < length -i - 1; j++) {
+let numbersCollection = new NumbersCollection([10, 3, -5, 0])
 
-                // if collection is an array of numbers
-                if (this.collection instanceof Array) {             // type guard
-                    // this.collection.push             // we can use Array specifyc operations inside the Type Guard block
-                    // so, at this point collection === number[]
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        let leftHand = this.collection[j]
-                        this.collection[j] = this.collection[j+1]
-                        this.collection[j+1] = leftHand
-                    }
-                }
-
-                if (typeof this.collection === 'string') { // type guard for collection of string
-
-                }
-
-
-                // if collection is an array of strings...
-            }
-        }
-    }
-}
-
-const sorter = new Sorter([10, 3, -5, 6])
+const sorter = new Sorter(numbersCollection)
 sorter.sort()
 console.log(sorter.collection)
