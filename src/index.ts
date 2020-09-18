912f2f83b91deb6abbee6dca6ceb8373128dfaac
Sergio Cano <serdeliverance@gmail.com>
2020-09-18 19:55:54 -0300

    refactor using abstract classes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
-import { Sorter } from './Sorter'
-import { NumbersCollection } from './NumbersCollection'
 import { CharactersCollection } from './CharactersCollection'
+import { LinkedList } from './LinkedList'
+import { NumbersCollection } from './NumbersCollection'
 
-// let numbersCollection = new NumbersCollection([10, 3, -5, 0])
+console.log('Sorting a numbers collection')
 
-// const sorter = new Sorter(numbersCollection)
-// sorter.sort()
-// console.log(sorter.collection)
+let numbersCollection = new NumbersCollection([10, 3, -5, 0])
+numbersCollection.sort()
+console.log(numbersCollection.data)
+
+console.log('Sorting a character collection')
 
 let charactersCollection = new CharactersCollection('caracteres')
-let sorter = new Sorter(charactersCollection)
-
-sorter.sort()
+charactersCollection.sort()
 console.log(charactersCollection.data)
+
+console.log('Sorting a linked list')
+
+let linkedList = new LinkedList()
+linkedList.add(500)
+linkedList.add(-3)
+linkedList.add(0)
+linkedList.add(45)
+linkedList.add(7)
+linkedList.sort()
+linkedList.print()
